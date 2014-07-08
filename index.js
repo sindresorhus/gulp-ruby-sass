bf55b781d7e7f34a8d706a52b9bdc72c985d3bb0
Alex Sanders <a@snd.rs>
2014-07-08 10:44:17 +0100

    Make loadPath relative to the gruntfile cwd

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var gutil = require('gulp-util');
 var spawn = require('win-spawn');
 var eachAsync = require('each-async');
 var intermediate = require('gulp-intermediate');
+var isString = require('lodash.isstring');
+var isArray = require('lodash.isstring');
 
 function rewriteSourcemapPaths (cssDir, relPath, cb) {
 	var glob = require('glob');
@@ -46,6 +48,10 @@ module.exports = function (options) {
 	var compileDir = '_14139e58-9ebe-4c0f-beca-73a65bb01ce9';
 	var procDir = process.cwd();
 	options = options || {};
+	options.loadPath = isString(options.loadPath) ? [options.loadPath] : options.loadPath;
+	options.loadPath = isArray(options.loadPath) ? options.loadPath.map(function(loadPath){
+		return path.join(procDir, loadPath);
+	}) : [];
 	options.cacheLocation = options.cacheLocation || path.join(procDir, '.sass-cache');
 	options.update = '.:' + compileDir;
 	var args = dargs(options, ['bundleExec', 'watch', 'poll', 'sourcemapPath']);
