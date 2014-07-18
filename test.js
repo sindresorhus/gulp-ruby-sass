f4280cdc879b7202a4676f9236f2c543e9e19ef3
Rob Wierzbowski <hello@robwierzbowski.com>
2014-07-18 01:11:42 -0400

    Break tests into smaller chunks
    This doesn't have a huge effect right now, but sets the stage for testing
    features more atomically (like the stream errors being added next).

--- test.js
+++ test.js
@@ -4,56 +4,85 @@ var gutil = require('gulp-util');
 var sass = require('./index');
 var EOL = require('os').EOL;
 
-var testFile = new gutil.File({
-	cwd: __dirname,
-	base: __dirname + '/styles',
-	path: __dirname + '/styles/nested/fixture.scss',
-	contents: new Buffer('$blue:#3bbfce;.content-navigation{border-color:$blue;}')
-});
-var files = [];
+var testFiles = [
+	new gutil.File({
+		cwd: __dirname,
+		base: __dirname + '/styles',
+		path: __dirname + '/styles/module/fixture-a.scss',
+		contents: new Buffer('@import \'../vars/fixture-b\'; aside {border-color:$blue;}')
+	}),
+	new gutil.File({
+		cwd: __dirname,
+		base: __dirname + '/styles',
+		path: __dirname + '/styles/vars/_fixture-b.scss',
+		contents: new Buffer('$blue:#3bbfce;')
+	})
+];
 
-it('should compile Sass with sourcemaps', function (cb) {
+it('should compile Sass', function (done) {
 	this.timeout(20000);
 
-	// Assume gulp.src('app/styles/**/*.scss')
-	// Assume gulp.dest('dist/styles')
-	// Assume connect().use(connect.static('app')).use(connect.static('.tmp'))
-
-	var stream = sass({
-		sourcemap: true,
-		sourcemapPath: '.',
-		quiet: true
-	});
+	var files = [];
+	var result = 'aside {' + EOL +
+		'  border-color: #3bbfce; }' + EOL;
+	var stream = sass({ quiet: true });
 
 	stream.on('data', function (file) {
-		files.push(file.relative);
-
-		// Test compiled CSS
-		if (/\.css$/.test(file.path)) {
-			assert.equal(file.relative, 'nested/fixture.css');
-			assert.equal(
-				file.contents.toString('utf-8'),
-				'.content-navigation {' + EOL + '  border-color: #3bbfce; }' + EOL + EOL + '/*# sourceMappingURL=fixture.css.map */' + EOL
-			);
-			return;
-		}
-
-		// Test sourcemap
-		if (/\.map$/.test(file.path)) {
-			var sourcemap = JSON.parse(file.contents.toString());
-			assert.equal(file.relative, 'nested/fixture.css.map');
-			assert.equal(sourcemap.version, 3);
-			assert.equal(sourcemap.file, 'fixture.css');
-			assert.equal(sourcemap.sources[0], '../nested/fixture.scss');
-		}
+		files.push(file);
 	});
 
 	stream.on('end', function () {
-		// make sure we've generated the expected files, and only those files
-		assert.deepEqual(files, [ 'nested/fixture.css', 'nested/fixture.css.map' ]);
-		cb();
+		// sass file
+		assert.equal(files[0].relative, 'module/fixture-a.css');
+		assert.equal(files[0].contents.toString('utf-8'), result);
+
+		// generates the correct number of files (does not output partials)
+		assert.equal(files.length, 1);
+
+		done();
 	});
 
-	stream.write(testFile);
+	stream.write(testFiles[0]);
+	stream.write(testFiles[1]);
+	stream.end();
+});
+
+it('should compile Sass with sourcemaps', function (done) {
+	this.timeout(20000);
+
+	var files = [];
+	var result = 'aside {' + EOL +
+    '  border-color: #3bbfce; }' + EOL + EOL +
+    '/*# sourceMappingURL=fixture-a.css.map */' + EOL;
+	var stream = sass({ sourcemap: true, sourcemapPath: '../scss', quiet: true });
+
+	stream.on('data', function (file) {
+		files.push(file);
+	});
+
+	stream.on('end', function () {
+		// sass file
+		assert.equal(files[0].relative, 'module/fixture-a.css');
+		assert.equal(files[0].contents.toString('utf-8'), result);
+
+		// sourcemap file
+		var sourcemap = JSON.parse(files[1].contents.toString());
+
+		assert.equal(files[1].relative, 'module/fixture-a.css.map');
+		assert.equal(sourcemap.version, 3);
+		assert.equal(sourcemap.file, 'fixture-a.css');
+		assert.deepEqual(sourcemap.sources,  [
+			'../../scss/module/fixture-a.scss',
+			'../../scss/vars/_fixture-b.scss'
+		]);
+
+		// generates the correct number of files
+		assert.equal(files.length, 2);
+
+		done();
+	});
+
+	stream.write(testFiles[0]);
+	stream.write(testFiles[1]);
 	stream.end();
 });
