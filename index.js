e473c5508f94015e9def1e3eec7b3ed495c040df
Rob Wierzbowski <hello@robwierzbowski.com>
2014-07-22 09:34:34 -0400

    Close GH-89: Automatically add loadpaths for intermediate files.

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var dargs = require('dargs');
 var slash = require('slash');
 var gutil = require('gulp-util');
 var spawn = require('win-spawn');
-var eachAsync = require('each-async');
 var intermediate = require('gulp-intermediate');
 
-function rewriteSourcemapPaths (cssDir, relPath, cb) {
+function rewriteSourcemapPaths (compileDir, relativePath, cb) {
+	var eachAsync = require('each-async');
 	var glob = require('glob');
 
-	glob(path.join(cssDir, '**/*.map'), function (err, files) {
+	glob(path.join(compileDir, '**/*.map'), function (err, files) {
 		if (err) {
 			cb(err);
 			return;
@@ -26,14 +26,14 @@ function rewriteSourcemapPaths (cssDir, relPath, cb) {
 				}
 
 				var sourceMap = JSON.parse(data);
-				var stepUp = path.relative(path.dirname(file), cssDir);
+				var stepUp = path.relative(path.dirname(file), compileDir);
 
 				// rewrite sourcemaps to point to the original source files
 				sourceMap.sources = sourceMap.sources.map(function (source) {
 					var sourceBase = source.replace(/\.\.\//g, '');
 
 					// normalize to browser style paths if we're on windows
-					return slash(path.join(stepUp, relPath, sourceBase));
+					return slash(path.join(stepUp, relativePath, sourceBase));
 				});
 
 				fs.writeFile(file, JSON.stringify(sourceMap, null, '  '), next);
@@ -42,60 +42,74 @@ function rewriteSourcemapPaths (cssDir, relPath, cb) {
 	});
 }
 
+function removePath(source, path) {
+	return source.replace(new RegExp(path + '/?', 'g'), '');
+}
+
 module.exports = function (options) {
-	var compileDir = '_14139e58-9ebe-4c0f-beca-73a65bb01ce9';
+	var relativeCompileDir = '_14139e58-9ebe-4c0f-beca-73a65bb01ce9';
 	var procDir = process.cwd();
-	options = options || {};
-
-	if (options.loadPath) {
-		options.loadPath = [].concat(options.loadPath).map(function (loadPath) {
-			return path.join(procDir, loadPath);
-		});
-	}
-
-	options.cacheLocation = options.cacheLocation || path.join(procDir, '.sass-cache');
-	options.update = '.:' + compileDir;
-	var args = dargs(options, ['bundleExec', 'watch', 'poll', 'sourcemapPath']);
-	var command;
 
 	// error handling
 	var noLogMatcher = /execvp\(\): No such file or directory/;
 	var bundleErrMatcher = /bundler: command not found|Could not find gem/;
 	var bundleErr = chalk.red('Gemfile version of Sass not found. Install missing gems with `bundle install`.');
 
-	if (options.bundleExec) {
-		command = 'bundle';
-		args.unshift('exec', 'sass');
-	} else {
-		command = 'sass';
-	}
-
 	return intermediate({
-		output: compileDir,
+		output: relativeCompileDir,
 		container: 'gulp-ruby-sass'
-	}, function (tempDir, cb, fileProps) {
+	}, function (tempDir, cb, vinylFiles) {
+
+		// all paths passed to sass must have unix path separators
+		tempDir = slash(tempDir);
+		var compileDir = slash(path.join(tempDir, relativeCompileDir));
+
+		options = options || {};
+		options.update = tempDir + ':' + compileDir;
+		options.loadPath = typeof options.loadPath === 'undefined' ? [] : [].concat(options.loadPath);
+
+		// add loadPaths for each temp file
+		vinylFiles.forEach(function (file) {
+			var loadPath = slash(path.dirname(path.relative(procDir, file.path)));
+
+			if (options.loadPath.indexOf(loadPath) === -1) {
+				options.loadPath.push(loadPath);
+			}
+		});
+
+		var args = dargs(options, ['bundleExec', 'watch', 'poll', 'sourcemapPath']);
+
+		// temporary logging until gulp adds its own
 		if (process.argv.indexOf('--verbose') !== -1) {
-			gutil.log('gulp-ruby-sass:', 'Running command:',
-				chalk.blue(command, args.join(' ')));
+			gutil.log('gulp-ruby-sass:', 'Running command:', chalk.blue(command, args.join(' ')));
 		}
 
-		var sass = spawn(command, args, {cwd: tempDir});
+		var command;
+
+		if (options.bundleExec) {
+			command = 'bundle';
+			args.unshift('exec', 'sass');
+		} else {
+			command = 'sass';
+		}
+
+		var sass = spawn(command, args);
 
 		sass.stdout.setEncoding('utf8');
 		sass.stderr.setEncoding('utf8');
 
 		sass.stdout.on('data', function (data) {
-			var msg = data.trim();
+			var msg = removePath(data, compileDir).trim();
 
 			if (bundleErrMatcher.test(msg)) {
 				gutil.log('gulp-ruby-sass:', bundleErr);
 			} else {
-				gutil.log('gulp-ruby-sass:', msg.replace(new RegExp(compileDir, 'g'), ''));
+				gutil.log('gulp-ruby-sass:', msg);
 			}
 		});
 
 		sass.stderr.on('data', function (data) {
-			var msg = data.trim();
+			var msg = removePath(data, tempDir).trim();
 
 			if (bundleErrMatcher.test(msg)) {
 				gutil.log('gulp-ruby-sass:', bundleErr);
@@ -121,9 +135,7 @@ module.exports = function (options) {
 			}
 
 			if (options.sourcemap && options.sourcemapPath) {
-				var cssDir = path.join(tempDir, compileDir);
-
-				rewriteSourcemapPaths(cssDir, options.sourcemapPath, function (err) {
+				rewriteSourcemapPaths(compileDir, options.sourcemapPath, function (err) {
 					if (err) {
 						this.emit('error', new gutil.PluginError('gulp-ruby-sass', err));
 					}
