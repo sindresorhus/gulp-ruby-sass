f8dec026efe35194ea65c1eacf4902157eb7be7b
Rob Wierzbowski <hello@robwierzbowski.com>
2014-11-14 16:52:05 -0500

    Add index.html and gulpfile.js for testing sourcemaps
    I've been using these, probably good to make available to everyone else.
    Directions on use in the html file.

---
+++ fixture/gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('../');
+
+// inline sourcemaps
+gulp.task('sass-inline', function() {
+	return sass('source', { sourcemap: true })
+	.on('error', function (err) {
+	  console.error('Error!', err.message);
+   })
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('result'));
+});
+
+// file sourcemaps
+gulp.task('sass-file', function() {
+	return sass('source', { sourcemap: true })
+	.on('error', function (err) {
+	  console.error('Error!', err.message);
+   })
+
+	.pipe(sourcemaps.write('../maps', {
+		includeContent: false,
+		sourceRoot: '/fixture/source'
+	}))
+
+	.pipe(gulp.dest('result'));
+});
